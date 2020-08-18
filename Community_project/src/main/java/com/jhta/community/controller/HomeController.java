c81cd1ea6b4afe5a595cd8856b91f8ed0e6ebf85
Amen0920 <luj0605@gmail.com>
2020-08-18 12:15:15 +0900

    master

---
+++ Community_project/src/main/java/com/jhta/community/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.jhta.community.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		System.out.println("1111");
+		
+		return "home";
+	}
+	
+}
